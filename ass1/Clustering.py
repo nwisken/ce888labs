d9b700a7058a323be54b3464ae09acb9c2521031
nwiske <nwiske@essex.ac.uk>
2019-04-22 19:37:03 +0100

    update

--- ass1/Clustering.py
+++ ass1/Clustering.py
@@ -1,29 +1,95 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from keras import Input
-from keras.layers import Dense
-from keras.models import Model
+from keras.models import Model, Sequential
+from keras.utils import to_categorical
 from sklearn.cluster import KMeans
-from sklearn.metrics import homogeneity_score, completeness_score, v_measure_score
 from sklearn.decomposition import PCA
-import matplotlib as plt
-import numpy as np
+from sklearn.metrics import homogeneity_score, completeness_score, v_measure_score
+from sklearn.model_selection import train_test_split
+from keras.layers import Dense, Dropout, Conv2D, MaxPool2D, UpSampling2D, Activation, Conv1D, MaxPool1D, UpSampling1D
+from keras import backend as K
+from sklearn.preprocessing import MinMaxScaler
 
-def cluster_autoencoder(train_data, train_label, num_classes):
+
+def plot_history(history, title):
+    """
+    Plots the  training and validation accuracy and loss in two graphs
+    :param history: History of classifier during training
+    """
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    #x = range(NUM_EPOCHS)
+
+    plt.figure(figsize=(12, 5))
+    plt.plot(loss, 'b', label='Training loss')
+    plt.plot(val_loss, 'r', label='Validation loss')
+    plt.title('{} Training and validation loss'.format(title))
+    plt.legend()
+    plt.show()
+
+
+def cluster_autoencoder(train_data, train_label, num_classes, title, num_epochs):
     batch_size = 128
-    num_epochs = 50
-    num_neurons_input = num_neurons_output = train_data.shape[1]
+    num_neurons_input = num_neurons_output = train_data.shape[1] # number features
     # num_neurons_hl1 = 35
-    num_neurons_hl1 = 50
+    num_neurons_hl1 = 128
+    num_neurons_hl2 = 65
+    num_neurons_hl3 = 32
+    train_data, val_data, train_label, val_label = train_test_split(train_data, train_label, test_size=0.1,
+                                                                    random_state=RANDOM_STATE)
 
     input_shape = Input(shape=(num_neurons_input,))
     # Dense = NN network layer
-    encoded = Dense(units=num_neurons_hl1, activation="relu")(input_shape)
-    decoded = Dense(units=num_neurons_output, activation="sigmoid")(encoded)
+    encoded = Dense(256, activation='relu')(input_shape)
+    encoded = Dense(128, activation='relu')(encoded)
+    encoded = Dense(64, activation='softmax')(encoded)
+
+    decoded = Dense(128, activation='relu')(encoded)
+    decoded = Dense(256, activation='relu')(decoded)
+    decoded = Dense(units=num_neurons_output, activation="sigmoid")(decoded)
     autoencoder = Model(input_shape, decoded)
     autoencoder.summary()
 
-    autoencoder.compile(optimizer="adadelta", loss="mse")
-    autoencoder.fit(train_data, train_data, batch_size=batch_size, epochs=num_epochs, shuffle=True)
+    autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
+    history = autoencoder.fit(train_data, train_data, batch_size=batch_size, epochs=num_epochs,
+                              validation_data=(val_data, val_data), verbose=0)
+    plot_history(history, title)
+
+    classifier = Dense(num_classes, activation="softmax")(encoded)
+    encoder = Model(input_shape, encoded)
+    encoded_data = encoder.predict(train_data)
+
+    cluster_preicdion = KMeans(n_clusters=num_classes, random_state=RANDOM_STATE).fit(encoded_data)
+
+    homo = homogeneity_score(train_label, cluster_preicdion.labels_)
+    comp = completeness_score(train_label, cluster_preicdion.labels_)
+    v = v_measure_score(train_label, cluster_preicdion.labels_)
+
+
+    return "homo: {} comp: {} v-Measure: {} ".format(homo, comp, v)
+
+def cluster_autoencoder_simple(train_data, train_label, num_classes, title, num_epochs):
+    batch_size = 128
+    num_neurons_input = num_neurons_output = train_data.shape[1] # number features
+    train_data, val_data, train_label, val_label = train_test_split(train_data, train_label, test_size=0.1,
+                                                                    random_state=RANDOM_STATE)
+
+    input_shape = Input(shape=(num_neurons_input,))
+    # "encoded" is the encoded representation of the input
+    encoded = Dense(50, activation='relu')(input_shape)
+    # "decoded" is the lossy reconstruction of the input
+    decoded = Dense(num_neurons_output, activation='sigmoid')(encoded)
+
+    autoencoder = Model(input_shape, decoded)
+    autoencoder.summary()
+
+
+    autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
+    history = autoencoder.fit(train_data, train_data, batch_size=batch_size, epochs=num_epochs,
+                              validation_data=(val_data, val_data), verbose=0, shuffle=True)
+    plot_history(history, title)
 
     encoder = Model(input_shape, encoded)
     encoded_data = encoder.predict(train_data)
@@ -34,18 +100,79 @@ def cluster_autoencoder(train_data, train_label, num_classes):
     comp = completeness_score(train_label, cluster_preicdion.labels_)
     v = v_measure_score(train_label, cluster_preicdion.labels_)
 
+
     return "homo: {} comp: {} v-Measure: {} ".format(homo, comp, v)
 
 
-def cluster_PCA(train_data, train_label, num_classes):
-    pca = PCA(.95)
+def cluster_conv(train_data, train_label, num_classes, title, num_epochs):
+    batch_size = 128
+    num_neurons_input = num_neurons_output = train_data.shape[1] # number features
+    # num_neurons_hl1 = 35
+    num_neurons_hl1 = 128
+    num_neurons_hl2 = 65
+    num_neurons_hl3 = 32
+    train_data, val_data, train_label, val_label = train_test_split(train_data, train_label, test_size=0.1,
+                                                                    random_state=RANDOM_STATE)
+
+    input_shape = Input(shape=(num_neurons_input,))
+    model = Sequential()
+    model.add(Conv1D(14, kernel_size=3, padding='same', activation='relu', input_shape=input_shape))
+    model.add(MaxPool1D((2, 2), padding='same'))
+    model.add(Dropout(0.2))
+    model.add(Conv1D(7, kernel_size=3, padding='same', activation='relu'))
+    model.add(MaxPool1D((2, 2), padding='same'))
+    model.add(Dropout(0.2))
+    model.add(Conv1D(7, kernel_size=3, padding='same', activation='relu'))
+    model.add(UpSampling1D((2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Conv1D(14, kernel_size=3, padding='same', activation='relu'))
+    model.add(UpSampling1D((2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Conv1D(1, kernel_size=3, padding='same', activation='relu'))
+
+    model.compile(optimizer='adam', loss="mse")
+    model.summary()
+
+
+
+    model.compile(optimizer="adam", loss="categorical_crossentropy")
+    history = model.fit(train_data, train_data, batch_size=batch_size, epochs=num_epochs,
+                              validation_data=(val_data, val_data), verbose=0)
+    plot_history(history, title)
+
+    encoder = K.function([model.layers[0].input], [model.layers[4].output])
+    encoded_data = encoder.predict(train_data)
+
+    cluster_preicdion = KMeans(n_clusters=num_classes, random_state=RANDOM_STATE).fit(encoded_data)
+    #one_hot_labels = to_categorical(train_label, num_classes=num_classes)
+
+    homo = homogeneity_score(train_label, cluster_preicdion.labels_)
+    comp = completeness_score(train_label, cluster_preicdion.labels_)
+    v = v_measure_score(train_label, cluster_preicdion.labels_)
+    #homo = homogeneity_score(one_hot_labels, cluster_preicdion.labels_)
+    #comp = completeness_score(one_hot_labels, cluster_preicdion.labels_)
+    #v = v_measure_score(train_label, cluster_preicdion.labels_)
+
+    return "homo: {} comp: {} v-Measure: {} ".format(homo, comp, v)
+
+
+def cluster_none(train_data, train_labels, num_classes):
+    clusters = KMeans(n_clusters=num_classes, random_state=RANDOM_STATE).fit(train_data)
+    homo = homogeneity_score(train_labels, clusters.labels_)
+    comp = completeness_score(train_labels, clusters.labels_)
+    v = v_measure_score(train_labels, clusters.labels_)
+    return "homo: {} comp: {} v-Measure: {} ".format(homo, comp, v)
+
+
+def cluster_PCA(train_data, train_label, num_classes, components):
+    pca = PCA(components)
     pca.fit(train_data)
 
     plt.figure()
     plt.plot(np.cumsum(pca.explained_variance_ratio_))
     plt.xlabel('Number of Components')
     plt.ylabel('Variance (%)')  # for each component
-    plt.title('Pulsar Dataset Explained Variance')
+    plt.title('Dataset Explained Variance')
     plt.show()
 
     train_data_pca = pca.transform(train_data)
@@ -67,9 +194,13 @@ RANDOM_STATE = 2472
 NUM_EPOCHS = 100
 BATCH_SIZE = 256
 
+NUM_CNAE = 9
+NUM_HAR = 6
+NUM_DIGIT = 10
+
 cnae = pd.read_csv(CNAE_9_PATH, header=None)
 cnae_data = cnae.drop(cnae.columns[0], axis=1)
-cnae_label = cnae[cnae.columns[0]]
+cnae_labels = cnae[cnae.columns[0]]
 
 har_data = pd.read_csv(HAR_DATA_PATH, delim_whitespace=True)
 har_labels = pd.read_csv(HAR_LABEL_PATH, delim_whitespace=True)
@@ -77,43 +208,52 @@ har_labels = har_labels.squeeze()
 
 digit = pd.read_csv(DIGIT_PATH)
 digit_data = digit.drop(digit.columns[0], axis=1)
-digit_label = digit[digit.columns[0]]
+digit_labels = digit[digit.columns[0]]
 
-cnae_clusters = KMeans(n_clusters=9, random_state=RANDOM_STATE).fit(cnae_data)
-cnae_homo = homogeneity_score(cnae_label, cnae_clusters.labels_)
-cnae_comp = completeness_score(cnae_label, cnae_clusters.labels_)
-cnae_v = v_measure_score(cnae_label, cnae_clusters.labels_)
+cnae_data = MinMaxScaler().fit_transform(cnae_data)
+digit_data = MinMaxScaler().fit_transform(digit_data)
+har_data = MinMaxScaler().fit_transform(har_data)
 
-#print("CNAE homo: {} comp: {} v-Measure: {} ".format(cnae_homo, cnae_comp, cnae_v))
+cnae_none = cluster_none(cnae_data, cnae_labels, NUM_CNAE)
+har_none = cluster_none(har_data, har_labels, NUM_HAR)
+digit_none = cluster_none(digit_data, digit_labels, NUM_DIGIT)
 
-har_clusters = KMeans(n_clusters=6, random_state=RANDOM_STATE).fit(har_data)
-har_homo = homogeneity_score(har_labels, har_clusters.labels_)
-har_comp = completeness_score(har_labels, har_clusters.labels_)
-har_v = v_measure_score(har_labels, har_clusters.labels_)
+digit_simple= cluster_autoencoder_simple(digit_data, digit_labels, 10,"MNIST digit",50)
+cnae_simple = cluster_autoencoder_simple(cnae_data, cnae_labels, 9,"CNAE-9", 50)
+har_simple = cluster_autoencoder_simple(har_data, har_labels, 6,"Human activity",50)
 
-#print("HAR homo: {} comp: {} v-Measure: {} ".format(har_homo, har_comp, har_v))
+#digit_results = cluster_autoencoder(digit_data, digit_labels, 10,"MNIST digit",30)
+#cnae_results = cluster_autoencoder(cnae_data, cnae_labels, 9,"CNAE-9",4)
+#har_results = cluster_autoencoder(har_data, har_labels, 6,"Human activity",5)
 
-digit_clusters = KMeans(n_clusters=10, random_state=RANDOM_STATE).fit(digit_data)
-digit_homo = homogeneity_score(digit_label, digit_clusters.labels_)
-digit_comp = completeness_score(digit_label, digit_clusters.labels_)
-digit_v = v_measure_score(digit_label, digit_clusters.labels_)
+#digit_conv = cluster_conv(digit_data, digit_labels, 10,"MNIST digit",50)
+#cnae_conv= cluster_conv(cnae_data, cnae_labels, 9,"CNAE-9",50)
+#har_conv = cluster_conv(har_data, har_labels, 6,"Human activity",50)
 
-#print("Digit homo: {} comp: {} v-Measure: {} ".format(digit_homo, digit_comp, digit_v))
+"""
+digit_PCA = cluster_PCA(digit_data, digit_labels, 10, 130)
+cnae_PCA = cluster_PCA(cnae_data, cnae_labels, 9, 250)
+har_PCA = cluster_PCA(har_data, har_labels, 6, 60)
+"""
 
-# digit_results = cluster_autoencoder(digit_data, digit_label, 10)
-# cnae_results = cluster_autoencoder(cnae_data, cnae_label, 9)
-# har_results = cluster_autoencoder(har_data, har_labels, 6)
+print("CNAE none", cnae_none)
+print("HAR none", har_none)
+print("Digit none", digit_none, '\n')
 
-digit_PCA = cluster_PCA(digit_data, digit_label, 10)
-cnae_PCA = cluster_PCA(cnae_data, cnae_label, 9)
-har_PCA = cluster_PCA(har_data, har_labels, 6)
+print("CNAE simple", cnae_simple)
+print("HAR simple", har_simple)
+print("Digit simple", digit_simple,'\n')
 
-print("CNAE homo: {} comp: {} v-Measure: {} ".format(cnae_homo, cnae_comp, cnae_v))
-print("HAR homo: {} comp: {} v-Measure: {} ".format(har_homo, har_comp, har_v))
-print("Digit homo: {} comp: {} v-Measure: {} ".format(digit_homo, digit_comp, digit_v))
-# print("CNAE with autoencoder", cnae_results)
-# print("HAR with autoencoder", har_results)
-# print("Digit with autoencoder", digit_results)
-print("CNAE with PCA", cnae_PCA)
-print("HAR with PCA", har_PCA)
-print("Digit with PCA", digit_PCA)
+#print("CNAE with autoencoder", cnae_results)
+#print("HAR with autoencoder", har_results)
+#print("Digit with autoencoder", digit_results,'\n')
+
+#print("CNAE with conv", cnae_conv)
+#print("HAR with conv", har_conv)
+#print("Digit with conv", digit_conv,'\n')
+
+"""
+print("CNAE with PCA", cnae_PCA)  # 120- 30
+print("HAR with PCA", har_PCA)  # 230-50
+print("Digit with PCA", digit_PCA)  # 60 pca
+"""
